dae10268c0097a4d41786cc113a5b71cd7b8c29b
vincenZo <vincenzo@Vees-MacBook-Pro.local>
2020-10-17 00:59:02 -0700

    Updates

---
+++ precache-manifest.4ae562c8dce94786f5d6956f5df770f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b1dcfa851eb5d099ce6799a8439f48",
+    "url": "/react-portfolio-build/index.html"
+  },
+  {
+    "revision": "5d2d70f500e467cd3e82",
+    "url": "/react-portfolio-build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "003a8411ff1438a2c71f",
+    "url": "/react-portfolio-build/static/js/2.6a69be80.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio-build/static/js/2.6a69be80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2d70f500e467cd3e82",
+    "url": "/react-portfolio-build/static/js/main.e71d2383.chunk.js"
+  },
+  {
+    "revision": "c476085f9d6a9cf6ba95",
+    "url": "/react-portfolio-build/static/js/runtime-main.e27800ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio-build/static/media/logo.5d5d9eef.svg"
+  }
+]);
